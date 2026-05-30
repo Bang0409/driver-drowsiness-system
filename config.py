68684ed4cc31d987c6b7959a5af3a601c96048b2
Bang0409 <dangcongbang978@gmail.com>
2026-05-30 10:25:40 +0700

    Add config file

---
+++ config.py
@@ -0,0 +1,17 @@
+# Ngưỡng phát hiện mắt nhắm
+EAR_THRESHOLD = 0.25
+
+# Thời gian nhắm mắt để cảnh báo
+CLOSED_EYES_TIME = 2
+
+# Ngưỡng phát hiện ngáp
+MAR_THRESHOLD = 0.6
+
+# Ngưỡng phát hiện gục đầu
+HEAD_DOWN_THRESHOLD = 0.15
+
+# Camera
+CAMERA_INDEX = 0
+
+# Âm thanh
+ALARM_SOUND = "sounds/alarm.mp3"
